f938f3c3cf625d91c82642a40df063672b4c33f8
Emma A. A. Rünzel <emma@ruenzel.de>
2023-11-14 08:01:56 +0100

    Seed DB w/ Test Data & Generate ChatroomsController

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+include ActionView::Helpers::TextHelper
+
+puts "Cleaning DB 🧼"
+
+User.destroy_all
+Message.destroy_all
+Chatroom.destroy_all
+
+Chatroom.create(name: "Bouldering for Beginners🧗🏼‍♀️")
+User.create(email: "iratxe@lewagon.com", nickname: "iratxe", password: "123456")
+User.create(email: "bernardo@lewagon.com", nickname: "bernardo", password: "123456")
+
+puts "Created #{pluralize(User.count, 'user')} and #{pluralize(Chatroom.count, 'chatroom')}"
